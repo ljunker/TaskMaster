7f8456f534411f38e9621ab491ab9368ad46022c
ljunker <larsjunker92@gmail.com>
2023-09-20 14:25:33 +0200

    loading tasks from json file

---
+++ src/main/Task.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type BadRequestError string
+
+type DateTime struct {
+	time time.Time
+}
+
+func (dt DateTime) format() string {
+	return time.RFC3339
+}
+func (dt DateTime) String() string {
+	return dt.time.Format(dt.format())
+}
+func (dt DateTime) MarshalJSON() ([]byte, error) {
+	return json.Marshal(dt.String())
+}
+func (dt DateTime) UnmarshalJSON(b []byte) error {
+	var s string
+	err := json.Unmarshal(b, &s)
+	if err != nil {
+		panic(BadRequestError("not a valid string"))
+	}
+	if s == "" {
+		panic(BadRequestError("must not be empty"))
+	}
+	t, err := time.Parse(dt.format(), s)
+	if err != nil {
+		panic(BadRequestError("format must be YYYY-MM-DDTHH:mm:ssZ"))
+	}
+	dt.time = t
+	return nil
+}
+
+type Task struct {
+	Id          uint64
+	Content     string
+	DateCreated DateTime
+}
+
+func (t Task) String() string {
+	return fmt.Sprintf("%d - %s - %s", t.Id, t.Content, t.DateCreated.String())
+}
--- src/main/main.go
+++ src/main/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello")
+	var tasks = loadTasks()
+	fmt.Println(tasks[0])
+}
+
+func loadTasks() []Task {
+	jsonFile, err := os.Open("tasks.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened tasks")
+	defer jsonFile.Close()
+
+	byteValue, _ := io.ReadAll(jsonFile)
+	var tasks []Task
+	err = json.Unmarshal(byteValue, &tasks)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return tasks
 }
